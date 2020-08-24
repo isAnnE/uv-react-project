589991379df8c319a3c44739b3a517d9123d98fd
Anne Morren <anne.morren@gmail.com>
2020-08-24 10:40:51 +0200

    update 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import './img/adelehaenelmodifs.png'
+import { Switch, Route } from "react-router-dom";
+
+// Pages
+import Home from "./components/page/Home";
+// import About from "./components/page/About";
+// import Contact from "./components/page/Contact";
 
 
 
 function App() {
   return (
     < div className="App">
-      <h1>Please work !!</h1>
-      <section id="slide">
-        <figure></figure>
-        <img src="../public/img/adelehaenelmodifs.png" alt="Adele Haenel Cesars 2020"></img>
-        <p id="catchphrase">Lieu de partage de films féministes</p>
-      </section>
-
-    </div>
+      {/* <Header /> */}
+      <main id="content-main">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          {/* <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/product/:id" component={Product} />
+          <Route path="/create-product" component={FormProduct} />
+          <Route path="*" component={NotFound} /> */}
+        </Switch>
+      </main>
+      {/* <Footer /> */}
+    </div >
   );
 }
 
