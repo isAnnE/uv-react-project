7ad679a8f825acb69fc17f9454c56267162fbeec
Anne Morren <anne.morren@gmail.com>
2020-09-02 17:26:21 +0200

    commit 2 septembre

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
+// import { Switch, Route } from "react-router-dom";
 import './App.scss';
 import './img/adelehaenelmodifs.png'
-// import { Switch, Route } from "react-router-dom";
+
 
 // Pages
-import Home from "./components/page/Home.js";
+// import Home from "./components/page/Home.js";
 // import About from "./components/page/About";
 // import Contact from "./components/page/Contact";
+// import Media from "./components/page/Media";
+// import NotFound from "./components/page/NotFound";
 
+// Layout
+import Header from "./components/layout/Header"
+import Footer from "./components/layout/Footer"
 
 
 function App() {
   return (
     < div className="App">
+      <Header />
       <main id="content-main">
-        <Home />
+
+        {/* <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/About" component={About} />
+        </Switch> */}
+
       </main>
+      <Footer />
     </div >
   );
 }
 
-
 export default App;
+
