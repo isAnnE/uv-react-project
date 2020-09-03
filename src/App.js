4cd67532205f92431e2f78e19da46811d3c6168d
Anne Morren <anne.morren@gmail.com>
2020-09-03 17:14:12 +0200

    commit 3 sept nav +  menuburger

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import { Switch, Route } from "react-router-dom";
 import './App.scss';
 import './img/adelehaenelmodifs.png'
+
+
 
 
 // Pages
@@ -9,28 +11,63 @@ import './img/adelehaenelmodifs.png'
 // import About from "./components/page/About";
 // import Contact from "./components/page/Contact";
 // import Media from "./components/page/Media";
-// import NotFound from "./components/page/NotFound";
+// 
 
 // Layout
 import Header from "./components/layout/Header"
 import Footer from "./components/layout/Footer"
+import SideDrawer from "./components/utils/sideDrawer";
+import Backdrop from "./components/utils/backdrop";
+import Navbar from "./components/utils/navbar";
+import { render } from '@testing-library/react';
+
+class App extends Component {
+
+  state = {
+    sideDrawerOpen: false
+  };
+
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen };
+    });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({ sideDrawerOpen: false });
+
+  }
+
+  render() {
+    let sideDrawer;
+    let backdrop;
+
+    if (this.state.sideDrawerOpen) {
+      sideDrawer = < SideDrawer />;
+      backdrop = < Backdrop click={this.backdropClickHandler} />
+    }
 
 
-function App() {
-  return (
-    < div className="App">
-      <Header />
-      <main id="content-main">
 
-        {/* <Switch>
+    return (
+      // < div className="App">
+      <div>
+        <Header />
+        <Navbar drawerClickHandler={this.drawerToggleClickHandler} />
+        {sideDrawer}
+        {backdrop}
+        <main id="content-main">
+          <p>this is my page content yo</p>
+          {/* <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/About" component={About} />
         </Switch> */}
 
-      </main>
-      <Footer />
-    </div >
-  );
+        </main>
+        <Footer />
+      </div >
+    );
+  }
 }
 
 export default App;
