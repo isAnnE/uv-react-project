017011d9a7dbafdc5a272014ba516cbfc7580bbb
Anne Morren <anne.morren@gmail.com>
2020-09-04 17:23:35 +0200

    friday evening 04-09

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 // import { Switch, Route } from "react-router-dom";
 import './App.scss';
-import './img/adelehaenelmodifs.png'
-
-
-
+// import './img/adelehaenelmodifs.png'
 
 // Pages
-// import Home from "./components/page/Home.js";
+import Home from "./components/page/Home.js";
 // import About from "./components/page/About";
 // import Contact from "./components/page/Contact";
 // import Media from "./components/page/Media";
-// 
 
 // Layout
 import Header from "./components/layout/Header"
@@ -19,7 +17,9 @@ import Footer from "./components/layout/Footer"
 import SideDrawer from "./components/utils/sideDrawer";
 import Backdrop from "./components/utils/backdrop";
 import Navbar from "./components/utils/navbar";
-import { render } from '@testing-library/react';
+import {
+  render
+} from '@testing-library/react';
 
 class App extends Component {
 
@@ -29,12 +29,16 @@ class App extends Component {
 
   drawerToggleClickHandler = () => {
     this.setState((prevState) => {
-      return { sideDrawerOpen: !prevState.sideDrawerOpen };
+      return {
+        sideDrawerOpen: !prevState.sideDrawerOpen
+      };
     });
   };
 
   backdropClickHandler = () => {
-    this.setState({ sideDrawerOpen: false });
+    this.setState({
+      sideDrawerOpen: false
+    });
 
   }
 
@@ -43,32 +47,47 @@ class App extends Component {
     let backdrop;
 
     if (this.state.sideDrawerOpen) {
-      sideDrawer = < SideDrawer />;
-      backdrop = < Backdrop click={this.backdropClickHandler} />
+      sideDrawer = < SideDrawer / > ;
+      backdrop = < Backdrop click = {
+        this.backdropClickHandler
+      }
+      />
     }
 
 
 
     return (
       // < div className="App">
-      <div>
-        <Header />
-        <Navbar drawerClickHandler={this.drawerToggleClickHandler} />
-        {sideDrawer}
-        {backdrop}
-        <main id="content-main">
-          <p>this is my page content yo</p>
-          {/* <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/About" component={About} />
-        </Switch> */}
+      <
+      div >
+      <
+      Header / >
+      <
+      Navbar drawerClickHandler = {
+        this.drawerToggleClickHandler
+      }
+      /> {
+        sideDrawer
+      } {
+        backdrop
+      } <
+      main id = "content-main" >
+      <
+      p > this is my page content yo < /p> {
+        /* <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/About" component={About} />
+                  </Switch> */
+      } <
+      Home / >
 
-        </main>
-        <Footer />
-      </div >
+      <
+      /main> <
+      Footer / >
+      <
+      /div >
     );
   }
 }
 
 export default App;
-
