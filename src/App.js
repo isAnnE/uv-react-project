1c03478b8cfb7a247bafe6bdbbf91fa9e741c918
Anne Morren <anne.morren@gmail.com>
2020-09-11 14:46:46 +0200

    commit 11 sept

--- src/App.js
+++ src/App.js
@@ -1,93 +1,205 @@
-import React, {
-  Component
-} from 'react';
-// import { Switch, Route } from "react-router-dom";
+// import React, {
+//   Component
+// } from 'react';
+import React from 'react';
 import './App.scss';
-// import './img/adelehaenelmodifs.png'
+// import axios from 'axios';
+
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import BrowserRouter as Router, { Link, NavLink, Switch, Route } from 'react-router-dom';
+import haenel from "./components/img/adelehaenelmodifs.png";
+
 
 // Pages
-import Home from "./components/page/Home.js";
-// import About from "./components/page/About";
-// import Contact from "./components/page/Contact";
-// import Media from "./components/page/Media";
+import About from "./components/page/About";
+// import Home from "./components/page/Home";
+import Media from "./components/page/Media";
+
 
 // Layout
-import Header from "./components/layout/Header"
-import Footer from "./components/layout/Footer"
-import SideDrawer from "./components/utils/sideDrawer";
-import Backdrop from "./components/utils/backdrop";
-import Navbar from "./components/utils/navbar";
-import {
-  render
-} from '@testing-library/react';
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 
-class App extends Component {
+// import { render } from '@testing-library/react';
 
-  state = {
-    sideDrawerOpen: false
-  };
+// COMPONENTS
+import Navbar from "./components/layout/nav/Navbar";
+// import { Router } from 'react-router';
 
-  drawerToggleClickHandler = () => {
-    this.setState((prevState) => {
-      return {
-        sideDrawerOpen: !prevState.sideDrawerOpen
-      };
-    });
-  };
-
-  backdropClickHandler = () => {
-    this.setState({
-      sideDrawerOpen: false
-    });
-
-  }
-
-  render() {
-    let sideDrawer;
-    let backdrop;
-
-    if (this.state.sideDrawerOpen) {
-      sideDrawer = < SideDrawer / > ;
-      backdrop = < Backdrop click = {
-        this.backdropClickHandler
-      }
-      />
-    }
+// const api = axios.create
 
 
-
-    return (
-      // < div className="App">
-      <
-      div >
-      <
-      Header / >
-      <
-      Navbar drawerClickHandler = {
-        this.drawerToggleClickHandler
-      }
-      /> {
-        sideDrawer
-      } {
-        backdrop
-      } <
-      main id = "content-main" >
-      <
-      p > this is my page content yo < /p> {
-        /* <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/About" component={About} />
-                  </Switch> */
-      } <
-      Home / >
-
-      <
-      /main> <
-      Footer / >
-      <
-      /div >
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Navbar />
+      <main>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/media" component={Media} />
+          </Switch>
+        </Router>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
+
+
+
+
+const Home = () => (
+  <>
+    <section id="slide" >
+      <figure>
+        <img src={haenel} alt="Adèle Haenel" className="haenel" />
+      </figure>
+
+
+      <p id="catchphrase" > Lieu de partage de films féministes </p>
+    </section>
+
+    <section id="intro">
+      <div id="intro">
+        <h1>Pourquoi Ultraviolettes ?</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorem voluptatibus corrupti,
+        assumenda repellendus,
+        numquam quasi officiis, officia laborum modi unde! Doloremque dolorem esse numquam quos earum quod vero
+            molestias?</p>
+      </div>
+    </section>
+
+    <section id="apercus">
+      <div id="apercus">
+        <figure class="apercu">Insert title</figure>
+        <figure class="apercu">Insert title</figure>
+        <figure class="apercu">Insert title</figure>
+      </div>
+    </section>
+
+  </>
+
+)
+
 export default App;
+
+
+
+//TUTO MAX
+
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import SideDrawer from "./components/utils/sideDrawer";
+// import Backdrop from "./components/utils/backdrop";
+// import Navbar from "./components/utils/navbar";
+
+// class App extends Component {
+
+//   state = {
+//     sideDrawerOpen: false
+//   };
+
+//   drawerToggleClickHandler = () => {
+//     this.setState((prevState) => {
+//       return {
+//         sideDrawerOpen: !prevState.sideDrawerOpen
+//       };
+//     });
+//   };
+
+//   backdropClickHandler = () => {
+//     this.setState({
+//       sideDrawerOpen: false
+//     });
+
+//   }
+
+//   render() {
+//     let sideDrawer;
+//     let backdrop;
+
+//     if (this.state.sideDrawerOpen) {
+//       sideDrawer = < SideDrawer />;
+//       backdrop = < Backdrop click={
+//         this.backdropClickHandler
+//       }
+//       />
+//     }
+
+
+
+//     return (
+//       <div>
+//         <Header />
+//         <Navbar drawerClickHandler={
+//           this.drawerToggleClickHandler
+//         }
+//         />
+//         {sideDrawer}
+//         {backdrop}
+//         {/* <Router>
+//           <div className="router">
+//             <Switch>
+//               <Route path="/" exact component={Home} />
+//               <Route path="/about" component={About} />
+//               <Route path="/shop" component={Media} />
+//             </Switch>
+//           </div>
+//         </Router> */}
+
+//         {/* <div className="router">
+//           <NavLink exact to="/">
+//             <Link to="/"> Home </Link>
+//             <Link to="/about"> About </Link> 
+
+//         </div> */}
+
+//         < main id="content-main" >
+//           <p > this is my page content yo </p>
+//           <Home />
+//         </main>
+//         <Footer />
+//       </div >
+//     );
+//   }
+// }
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <Navbar
+// drawerClickHandler={
+//   this.drawerToggleClickHandler
+// }
+// />
+/* {sideDrawer}
+      {backdrop} */
+/* <Router>
+        <div className="router">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/media" component={Media} />
+          </Switch>
+        </div>
+      </Router> */
+
+/* <div className="router">
+              <NavLink exact to="/">
+                <Link to="/"> Home </Link>
+                <Link to="/about"> About </Link>
+
+            </div> */
+
+//       < main id="content-main" >
+//         <p > this is my page content yo </p>
+//         <Home />
+//       </main>
+//       <Footer />
+//     </div >
+//   );
+// }
